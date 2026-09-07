ab7ba2764cb9f32b560a6b2b5390932b3e6792f2
EduardoAbreu01 <212421299+EduardoAbreu01@users.noreply.github.com>
2026-09-07 15:55:06 -0300

    fix: bug09 impede aluguel de conteudo indisponivel lancando ConteudoIndisponivelException

--- src/main/java/br/com/fiap/streamfiap/controller/AluguelController.java
+++ src/main/java/br/com/fiap/streamfiap/controller/AluguelController.java
@@ -1,6 +1,7 @@
 package br.com.fiap.streamfiap.controller;
 
 import br.com.fiap.streamfiap.exception.ClassificacaoIndicativaException;
+import br.com.fiap.streamfiap.exception.ConteudoIndisponivelException;
 import br.com.fiap.streamfiap.exception.ConteudoNaoEncontradoException;
 import br.com.fiap.streamfiap.exception.UsuarioNaoEncontradoException;
 import br.com.fiap.streamfiap.model.Conteudo;
@@ -29,7 +30,9 @@ public class AluguelController {
                 .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado"));
         Conteudo conteudo = conteudoRepository.findById(conteudoId)
                 .orElseThrow(() -> new ConteudoNaoEncontradoException("Conteúdo não encontrado: " + conteudoId));
-
+        if(!conteudo.isDisponivel()){
+            throw new ConteudoIndisponivelException("Conteúdo indisponível no momento!");
+        }
         Usuario usuarioAtualizado = usuario.alugar(conteudo);
 
         conteudoRepository.save(conteudo);
